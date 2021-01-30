756329fac4186fc0d2dd7bcf837e559dbdbbf25b
samiartur <samiartur@gmail.com>
2021-01-30 15:55:24 -0300

    Create cv01_switch.jsx
    This is a simple script to test fonts with Character Variants features like Adobe Souce Code Pro

---
+++ cv01_switch.jsx
@@ -0,0 +1,18 @@
+﻿// This script  was created by Sami A. Mandelbaum - Samtype Foundry - sami@samtype.com.br
+
+if( app.selection.length ) // do we have a selection?
+
+if( app.selection[0].hasOwnProperty("opentypeFeatures") )
+
+if( app.selection[0].opentypeFeatures.toSource()=="[[\"cv01\", 1]]" )
+
+    app.selection[0].opentypeFeatures = []; // reset to nothing
+
+else
+
+   app.selection[0].opentypeFeatures=[["cv01",1]]; // set them!
+
+
+// app.selection[0].opentypeFeatures=[["cv"+("00"+prompt("character variant","01")).slice(-2),1]];
+
+// if( app.selection[0].opentypeFeatures.toSource()=="[[\"cv01\", 1]]" )
